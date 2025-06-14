fb4e258ec45fa54e46e5ac3124607264953f760f
Luisgatovolador <rodriguez.ruisu@gmail.com>
2025-06-13 19:50:07 -0600

    Desafio 2

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <>
             <NavBar />
             <div style={{ padding: '20px' }}>
-                <h2>Bienvenido a mi página web sobre mis Mascotas  Desafio 2</h2>
+                <h2>Bienvenido a mi página web sobre mis Mascotas Actividad 4 y Desafio 2</h2>
                 <hr />
                 <p>Esta simple aplicación fue creada con React donde puedes Patrocinar a mis mascotas </p>
             </div>
